a702bc5f4a31a099a22be65117916f83a7e6d712
Jan Vigar <jansvigar@gmail.com>
2018-03-18 00:32:08 -0700

    add Navigation

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var menuIcon = document.querySelector('#menu-icon');
+var menuOverlay = document.querySelector('.site-navigation.overlay');
+var menuOverlayCloseIcon = document.querySelector('.site-navigation .close-icon');
+
+menuIcon.addEventListener("click", handleMenuOpen);
+
+
+function handleMenuOpen() {
+    menuOverlay.className += ' open';
+    menuOverlayCloseIcon.addEventListener("click", handleMenuClose);
+}
+
+function handleMenuClose() {
+    menuOverlay.className = menuOverlay.className.replace(/\bopen\b/g, '');
+}
