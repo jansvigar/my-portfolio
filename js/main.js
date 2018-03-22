c8c142ac5734108bf75fe8ed3cdfecb3494e4f3a
Jan Vigar <jansvigar@gmail.com>
2018-03-21 23:32:25 -0700

    add about me page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
-var logo = document.querySelector('.brand');
 var menuIcon = document.querySelector('#menu-icon');
 var menuOverlay = document.querySelector('.site-navigation.overlay');
-var menuNavigationItems = document.querySelectorAll('.site-navigation>li');
 var menuOverlayCloseIcon = document.querySelector('.site-navigation .close-icon');
-var homeButton = document.querySelector('section.home button>a');
+var scrollElements = document.querySelectorAll('.scroll');
 var scrollInterval;
-
-console.log(logo.children[0].getAttribute('href'));
 
 menuIcon.addEventListener('click', openMenu);
 
-for(var i = 0; i < menuNavigationItems.length; i++) {
-    menuNavigationItems[i].addEventListener('click', handleNavigationItemClick);
+for(var i = 0; i < scrollElements.length; i++) {
+    scrollElements[i].addEventListener('click', handleNavigationItemClick);
 }
-
-logo.addEventListener('click', handleNavigationItemClick);
-homeButton.addEventListener('click', handleNavigationItemClick);
 
 function handleNavigationItemClick(e) {
     e.preventDefault();
     var target = e.currentTarget.getAttribute('href') || e.currentTarget.children[0].getAttribute('href');
-    console.log(target);
     if(history.pushState) {
         history.pushState(null, null, target);
     }
@@ -39,6 +31,10 @@ function closeMenu() {
     menuOverlay.className = menuOverlay.className.replace(/\bopen\b/g, '');
 }
 
+/**
+ * Function to animate smooth scrolling to the destination. 
+ * inspired by http://stackoverflow.com/a/24559613/728480.
+ */
 function scrollTo(destinationInPixel, durationInMs) {
     if(scrollInterval) clearInterval(scrollInterval);
 
