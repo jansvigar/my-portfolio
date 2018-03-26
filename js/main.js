ef431070689bd258e6c5763bbaeaf8bdc2c4c59f
Jan Vigar <jansvigar@gmail.com>
2018-03-25 22:51:18 -0700

    fix modal open animation

--- js/main.js
+++ js/main.js
@@ -35,7 +35,6 @@ for(var i = 0; i < scrollElements.length; i++) {
 }
 
 function showModal(modal) {
-    modal.className = modal.className.replace(/\bhidden\b/g, '');
     modal.className = 'modal open';
 }
 
