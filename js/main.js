d7992d9cfd3890708d449bc6f3c3eba0bf6784e7
Jan Vigar <jansvigar@gmail.com>
2018-03-27 23:46:40 -0700

    add more accessibility

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+var body = document.querySelector("body");
 var menuIcon = document.querySelector("#menu-icon");
 var menuOverlay = document.querySelector(".site-navigation.overlay");
 var menuOverlayLinks = document.querySelectorAll(".site-navigation.overlay > ul > li > a");
 var menuOverlayCloseIcon = document.querySelector(
   ".site-navigation .close-icon"
 );
+var lastFocus;
 
 for(var i = 0; i < menuOverlayLinks.length; i++) {
     var link = menuOverlayLinks[i];
@@ -34,26 +36,68 @@ for (var i = 0; i < modalElements.length; i++) {
 
 window.addEventListener("click", function(e) {
   if (e.target.className === "modal open") {
-    e.target.className = "modal";
+    closeModal(e.target);
   }
+});
+
+document.addEventListener("keydown", function(e) {
+    if(!e.keyCode || e.keyCode === 27) {
+        var openedModal = e.currentTarget.querySelector(".modal.open");
+        if(openedModal) closeModal(openedModal);
+        var openedMenu = e.currentTarget.querySelector(".site-navigation.overlay.open");
+        if(openedMenu) closeMenu();
+    }
 });
 
 menuIcon.addEventListener("click", openMenu);
 
+menuIcon.addEventListener("keyup", function(e) {
+    e.preventDefault();
+    if(!e.keyCode || e.keyCode === 13) {
+        menuIcon.click();
+    }
+})
+
+menuOverlayCloseIcon.addEventListener("keyup", function(e) {
+    e.preventDefault();
+    if(!e.keyCode || e.keyCode === 13) {
+        closeMenu();
+    }
+})
+
 function showModal(modal) {
   modal.className = "modal open";
+  lastFocus = document.activeElement;
+  modal.querySelector(".modal-content").focus();
+  body.className = "noscroll";
+  body.addEventListener("touchmove", function(e) {
+        e.preventDefault();
+  });
 }
 
 function closeModal(modal) {
   modal.className = "modal";
+  body.className = "";
+  body.removeEventListener("touchmove", function(e) {
+    e.preventDefault();
+  });
+  lastFocus.focus();
 }
 
 function openMenu() {
   menuOverlay.className += " open";
   menuOverlay.className = menuOverlay.className.replace(/\bhidden\b/g, "");
+  body.className = "noscroll";
   menuOverlayCloseIcon.addEventListener("click", closeMenu);
+  body.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+});
 }
 
 function closeMenu() {
   menuOverlay.className = menuOverlay.className.replace(/\bopen\b/g, "");
+  body.className = "";
+  body.removeEventListener("touchmove", function(e) {
+    e.preventDefault();
+  });
 }
